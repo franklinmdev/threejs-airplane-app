dd2eb10174bed72fb08f13171f9dea1225b290bb
TheMVProgrammer <franklin0415@hotmail.com>
2022-07-24 12:44:19 -0400

    Adding javascript changes

--- js/main.js
+++ js/main.js
@@ -6,7 +6,13 @@ const hiddenInput1 = document.querySelector('#inputSelect1');
 const hiddenInput2 = document.querySelector('#inputSelect2');
 
 document.querySelectorAll('.options > .option-select').forEach((option) => {
-    console.log('hola');
+    option.addEventListener('click', (e) => {
+        e.preventDefault();
+        contentSelect.innerHTML = e.currentTarget.innerHTML;
+        select.classList.toggle('active');
+        options.classList.toggle('active');
+        hiddenInput1.value = e.currentTarget.querySelector('.title').innerText;
+    });
 });
 
 select.addEventListener('click', () => {
@@ -18,7 +24,13 @@ const select2 = document.querySelector('.select2');
 const options2 = document.querySelector('.options2');
 
 document.querySelectorAll('.options2 > .option-select').forEach((option) => {
-    console.log('hola');
+    option.addEventListener('click', (e) => {
+        e.preventDefault();
+        contentSelect2.innerHTML = e.currentTarget.innerHTML;
+        select2.classList.toggle('active');
+        options2.classList.toggle('active');
+        hiddenInput2.value = e.currentTarget.querySelector('.title').innerText;
+    });
 });
 
 select2.addEventListener('click', () => {
