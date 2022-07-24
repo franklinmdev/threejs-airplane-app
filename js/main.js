c1ada08f041c32b39cbff7954cd5773cab103821
TheMVProgrammer <franklin0415@hotmail.com>
2022-07-24 10:51:07 -0400

    Adding querySelector for content-select and hiddenInput

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 const select = document.querySelector('.select');
 const options = document.querySelector('.options');
+const contentSelect = document.querySelector('.select .content-select');
+const contentSelect2 = document.querySelector('.select2 .content-select');
+const hiddenInput1 = document.querySelector('#inputSelect1');
+const hiddenInput2 = document.querySelector('#inputSelect2');
+
+document.querySelectorAll('.options > .option-select').forEach((option) => {
+    console.log('hola');
+});
 
 select.addEventListener('click', () => {
     select.classList.toggle('active');
@@ -8,6 +16,10 @@ select.addEventListener('click', () => {
 
 const select2 = document.querySelector('.select2');
 const options2 = document.querySelector('.options2');
+
+document.querySelectorAll('.options2 > .option-select').forEach((option) => {
+    console.log('hola');
+});
 
 select2.addEventListener('click', () => {
     select2.classList.toggle('active');
